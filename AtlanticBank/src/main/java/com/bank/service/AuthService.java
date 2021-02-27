34b7d12d1c1c77c6ed65ca01f3dfc7a167c24696
Gael22 <gohoungogael@gmail.com>
2021-02-27 11:08:05 -0500

    created Authentication Service

--- AtlanticBank/src/main/java/com/bank/service/AuthService.java
+++ AtlanticBank/src/main/java/com/bank/service/AuthService.java
@@ -1,5 +1,12 @@
 package com.bank.service;
 
+import com.bank.pojo.User;
+
 public interface AuthService {
 
+	public boolean exixtingUser(User user);
+	
+	public User authenticateUser(User user);
+	
+	public User registerUser(User user);
 }
